ae52aea6cd52e5cec288c8700ade2aae4242d2bc
rvlouie <rvlouie@gmail.com>
2014-05-23 15:28:02 -0400

    Init commit

---
+++ js/converse-about.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+  $(function() {
+    $("a").click(function() {
+       if(this.hash) {
+        var hash = this.hash.substr(1);
+        var jQuerytoElement = jQuery("div[id="+hash+"]");
+        var toPosition = jQuerytoElement.position().top;
+        jQuery("body,html").animate({
+            scrollTop : toPosition
+        }, 800);
+        return false;            
+       }
+    });
+    if(location.hash) {
+        var hash = location.hash;
+        window.scroll(0,0);
+        jQuery("a[href="+hash+"]").click();
+    }
+  });
+
+  $(".arrow a").hover(function() {
+    $(this).css("background-color", "rgb(55, 55, 55)")
+  }, function() {
+    $(this).css("background-color", "rgb(88, 88, 88)")
+  });
+
+})
